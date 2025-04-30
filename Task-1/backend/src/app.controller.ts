746d1fd7ef8e23461ef421969f351d8d9f4c7e1f
FurkannOzbek <furkannozbek@yandex.com>
2025-04-30 21:09:37 +0200

    [feat] Logs for recieved message and timestamp added

--- Task-1/backend/src/app.controller.ts
+++ Task-1/backend/src/app.controller.ts
@@ -1,20 +1,25 @@
-import { Controller, Get , Post, Body, BadRequestException} from '@nestjs/common';
+import { Controller, Get , Post, Body, BadRequestException, Logger} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AskAiDto } from './dto/askAi.dto';
 
 @Controller()
 export class AppController {
+  // Initilazing logger for logging messages
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
-
+ // Default endpoint to check if the server is running
   @Get()
   getHello(): string {
+    
     return this.appService.getHello();
   }
+
+  // AskAi endpoint to get AI response
   @Post('askAi')
   askAi(@Body() askAiDto: AskAiDto): string {
     if(!askAiDto.message) {
       throw new BadRequestException('Message should not be empty');}
-    
+    this.logger.log(`Received message:${askAiDto.message} at ${new Date()}`);
     return this.appService.askAi(askAiDto.message);
   }
 }
--- Task-1/backend/src/app.service.ts
+++ Task-1/backend/src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
     return 'Welcome to Digibate Internship Task 1 API';
   }
   askAi(message: string): string {
+ 
     return `AI response to: ${message}`;
   }
 }
