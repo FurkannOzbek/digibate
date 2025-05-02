e4e7a09deed91bc43e74ebc72530d1a0917278d4
FurkannOzbek <furkannozbek@yandex.com>
2025-05-02 13:35:37 +0200

    [FEAT] landingpage component added, global css styles updated

--- Task-1/frontend/src/app/app.component.ts
+++ Task-1/frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LandingpageComponent } from './landingpage/landingpage.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
