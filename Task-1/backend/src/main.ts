6a6382e1257ea901b30f72ec7e2639eacbd80371
FurkannOzbek <furkannozbek@yandex.com>
2025-05-03 13:24:45 +0200

    [feat] cors enabled for prevent conflict

--- Task-1/backend/src/main.ts
+++ Task-1/backend/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Cors enabled for prevent conflicts frontend-backend communication
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
