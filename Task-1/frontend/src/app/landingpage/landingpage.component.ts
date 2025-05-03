a5bdecc500892c8f34d9459e52a6ab4dd63f027a
FurkannOzbek <furkannozbek@yandex.com>
2025-05-03 13:19:15 +0200

    [feat] frontend api call for generate content  added

--- Task-1/frontend/src/app/landingpage/landingpage.component.ts
+++ Task-1/frontend/src/app/landingpage/landingpage.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon'; 
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [ MatButtonModule,MatIconModule],
+  imports: [ MatButtonModule,MatIconModule,FormsModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
 export class LandingpageComponent {
+  promptInput= "";
+  isLoading = false;
+  result = "";
 
+   constructor(private apiService: ApiService) { } 
+   generateTopic() {
+    if(!this.promptInput.trim()) return;
+    this.isLoading = true;
+    this.apiService.generateContent(this.promptInput).subscribe({
+      next: (response) => {
+        this.result = response;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error("Error generating content:", error);
+        this.isLoading = false;
+      }
+     });
+   }
 }
